e68fcd798c3f6d1483f86f73cb4b54b6ce0c06d5
Kenneth Shaw <kenshaw93@gmail.com>
2014-02-20 13:33:29 +0100

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Groupify::Application.config.secret_key_base = '722fa1ed988a567bb9d99530a38cec41bb108b5643e3f9931cdbd5e71c399ae79231ce38f4a01122187d207fd9b1bc868810aa33e3f654d17aba80413ad64a32'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Groupify::Application.config.session_store :cookie_store, key: '_groupify_session'
